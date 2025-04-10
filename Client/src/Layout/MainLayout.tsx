5fc0f0facb5716d02e8afe8493b3c686c26280fb
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 09:21:06 +0200

    Update: MainLayout layout logic

--- Client/src/Layout/MainLayout.tsx
+++ Client/src/Layout/MainLayout.tsx
@@ -1,5 +1,19 @@
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import WithTheme from "../Utils/WithTheme";
+
 const MainLayout = () => {
-  return <div></div>;
+  const ThemedHeader = WithTheme(Header);
+  const ThemedFooter = WithTheme(Footer);
+  const ThemedOutlet = WithTheme(Outlet);
+  return (
+    <>
+      <ThemedHeader />
+      <ThemedOutlet />
+      <ThemedFooter />
+    </>
+  );
 };
 
 export default MainLayout;
