a8e58f03c1caca0ff3c08e3ec531a9e678dbede2
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 09:21:23 +0200

    Add: Pages module with route components

---
+++ Client/src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <div className="w-full flex justify-center">
+        <div className="text-red-500 w-fit bg-[#f3f3f3] font-semibold rounded-full px-3 py-1">
+          <h1>No.1 Job Log Website</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
