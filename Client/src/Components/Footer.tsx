7cc40f9ff089c63f61807bbda3b18f0e99ce6cf7
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 09:21:14 +0200

    Add: Footer component

---
+++ Client/src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <h1 className="text-center">
+        © 2025 Mohamed Osama. All rights reserved.
+      </h1>
+    </>
+  );
+};
+
+export default Footer;
