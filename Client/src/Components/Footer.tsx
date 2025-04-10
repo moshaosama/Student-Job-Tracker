7f84feab5c7b782f1cae0653a7f54b6c31f3efc1
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 10:23:42 +0200

    Update: Footer component enhancements

--- Client/src/Components/Footer.tsx
+++ Client/src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <h1 className="text-center">
+      <h1 className="text-center pt-5">
         © 2025 Mohamed Osama. All rights reserved.
       </h1>
     </>
