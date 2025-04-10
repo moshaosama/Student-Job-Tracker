c417efd4fbd55014c4c5b855b29ea550fb4017ad
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 11:06:15 +0200

    Modify server.js

---
+++ Server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./ConnectDB/DB.js";
+import createJopRouter from "./Router/Jop/createJop.js";
+import getJopRouter from "./Router/Jop/getJop.js";
+const app = express();
+const Port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config({ path: ".env" });
+
+// EndPoints
+app.use("/createjop", createJopRouter);
+app.use("/getjops", getJopRouter);
+
+////////////
+
+app.listen(Port, async () => {
+  try {
+    connectDB();
+    console.log("Successfully connect on port", Port);
+  } catch (error) {
+    console.log("Failed Connected on port", error);
+  }
+});
