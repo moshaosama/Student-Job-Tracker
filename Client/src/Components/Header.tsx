685b0c33857c052ca9929118a83136b673b72617
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 13:43:58 +0200

    update: updated Header component

--- Client/src/Components/Header.tsx
+++ Client/src/Components/Header.tsx
@@ -1,19 +1,22 @@
 import { MdDarkMode } from "react-icons/md";
 import { container } from "../Style";
 import useTheme from "../Hooks/useTheme";
+import { Link } from "react-router";
 
 const Header = () => {
   const { handleChangeTheme } = useTheme();
   return (
     <div className={container}>
       <div>
-        <h1 className="font-bold text-2xl">
-          Job<span className="text-red-500">Log</span>
-        </h1>
+        <Link to={"/"}>
+          <h1 className="font-bold text-2xl">
+            Job<span className="text-red-500">Log</span>
+          </h1>
+        </Link>
       </div>
       <div className="flex items-center gap-5 font-semibold">
-        <a href="#">Home</a>
-        <a href="#">Jops</a>
+        <a href="/">Home</a>
+        <a href="/jops">Jops</a>
         <i onClick={handleChangeTheme}>
           <MdDarkMode className="text-xl cursor-pointer" />
         </i>
