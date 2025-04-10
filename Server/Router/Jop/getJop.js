94f3e4ec8046b2a3a459875bb3d70ce42ec87f2e
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 13:43:59 +0200

    update: improved getJop router handling

--- Server/Router/Jop/getJop.js
+++ Server/Router/Jop/getJop.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { getJop } from "../../Controller/Jop/getJops.js";
+import { getJop, getJopById } from "../../Controller/Jop/getJops.js";
 const getJopRouter = express.Router();
+
+const getJopByIdRouter = express.Router();
 
 getJopRouter.route("/").get(getJop);
 
-export default getJopRouter;
+getJopByIdRouter.route("/:id").get(getJopById);
+
+export { getJopRouter, getJopByIdRouter };
