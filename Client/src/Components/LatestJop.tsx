56ee07da5021c18549e9441b16fc3c37b9390cbb
moshaosama <mohamedOSFekry@gmail.com>
2025-04-10 11:47:23 +0200

    Update LatestJop component

--- Client/src/Components/LatestJop.tsx
+++ Client/src/Components/LatestJop.tsx
@@ -1,6 +1,12 @@
+import useLoading from "../Hooks/useLoading";
 import { content } from "../Style";
+import WithLoader from "../Utils/WithLoader";
+import Jopinfo from "./Jopinfo";
+import GetJopsLoader from "./Shared/Jops/GetJopsLoader";
 
 const LatestJop = () => {
+  const LoadedJopInfo = WithLoader(Jopinfo);
+  const { loading } = useLoading();
   return (
     <>
       <div className={`${content}`}>
@@ -8,91 +14,9 @@ const LatestJop = () => {
           <span className="text-purple-400">Latest and Top</span> Job Logs
         </h1>
 
-        <div className="grid grid-cols-3 w-full gap-y-5">
-          <div className="border-[1px] cursor-pointer hover:bg-gray-100 border-gray-200 border-solid flex shadow-lg flex-col gap-1  rounded-lg px-5 py-2 w-96">
-            <h1 className="font-bold">Google</h1>
-            <p className="text-[#969696]">india</p>
-            <h1 className="font-bold">Full Stack Developer</h1>
-            <p className="text-[#969696]">
-              i need senior FullStack Developer, who can able to write the
-              effiecient code.
-            </p>
-            <div className="flex gap-2 items-center mt-4">
-              <p className="text-blue-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Interview
-              </p>
-              <p className="text-orange-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Full Time
-              </p>
-              <p className="text-purple-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Date
-              </p>
-            </div>
-          </div>
-
-          {/* ////////////// */}
-
-          <div className="border-[1px] cursor-pointer hover:bg-gray-100 border-gray-200 border-solid flex shadow-lg flex-col gap-1  rounded-lg px-5 py-2 w-96">
-            <h1 className="font-bold">Google</h1>
-            <p className="text-[#969696]">india</p>
-            <h1 className="font-bold">Full Stack Developer</h1>
-            <p className="text-[#969696]">
-              i need senior FullStack Developer, who can able to write the
-              effiecient code.
-            </p>
-            <div className="flex gap-2 items-center mt-4">
-              <p className="text-blue-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Interview
-              </p>
-              <p className="text-orange-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Full Time
-              </p>
-              <p className="text-purple-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Date
-              </p>
-            </div>
-          </div>
-          <div className="border-[1px] cursor-pointer hover:bg-gray-100 border-gray-200 border-solid flex shadow-lg flex-col gap-1  rounded-lg px-5 py-2 w-96">
-            <h1 className="font-bold">Google</h1>
-            <p className="text-[#969696]">india</p>
-            <h1 className="font-bold">Full Stack Developer</h1>
-            <p className="text-[#969696]">
-              i need senior FullStack Developer, who can able to write the
-              effiecient code.
-            </p>
-            <div className="flex gap-2 items-center mt-4">
-              <p className="text-blue-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Interview
-              </p>
-              <p className="text-orange-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Full Time
-              </p>
-              <p className="text-purple-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Date
-              </p>
-            </div>
-          </div>
-          <div className="border-[1px] cursor-pointer hover:bg-gray-100 border-gray-200 border-solid flex shadow-lg flex-col gap-1  rounded-lg px-5 py-2 w-96">
-            <h1 className="font-bold">Google</h1>
-            <p className="text-[#969696]">india</p>
-            <h1 className="font-bold">Full Stack Developer</h1>
-            <p className="text-[#969696]">
-              i need senior FullStack Developer, who can able to write the
-              effiecient code.
-            </p>
-            <div className="flex gap-2 items-center mt-4">
-              <p className="text-blue-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Interview
-              </p>
-              <p className="text-orange-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Full Time
-              </p>
-              <p className="text-purple-400 border-[1px] border-solid px-2 font-bold rounded-full w-fit">
-                Date
-              </p>
-            </div>
-          </div>
-        </div>
+        <GetJopsLoader resourceName="jops">
+          <LoadedJopInfo loading={loading} />
+        </GetJopsLoader>
       </div>
     </>
   );
