c20f765b75702bde5c76ba789a445ce37832411a
moshaosama <mohamedOSFekry@gmail.com>
2025-04-11 19:45:04 +0200

    Latest Version

--- Server/ConnectDB/DB.js
+++ Server/ConnectDB/DB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/JobLog");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
