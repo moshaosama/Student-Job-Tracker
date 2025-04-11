f59d4a3a2eb1eb1b018c18cb411be2a3acfc4cc4
moshaosama <mohamedOSFekry@gmail.com>
2025-04-11 18:58:22 +0200

    Latest Version

--- Server/server.js
+++ Server/server.js
@@ -7,28 +7,25 @@ import { getJopByIdRouter, getJopRouter } from "./Router/Jop/getJop.js";
 import EditJopRouter from "./Router/Jop/EditJop.js";
 import DeletedRouter from "./Router/Jop/DeletedJop.js";
 import FilterJopRouter from "./Router/Jop/FilterJopByFilter.js";
-const app = express();
-const Port = process.env.PORT || 3000;
 
+dotenv.config({ path: ".env" });
+
+const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-dotenv.config({ path: ".env" });
 
-// EndPoints
+// Routes
 app.use("/createjop", createJopRouter);
 app.use("/getjops", getJopRouter);
 app.use("/jop", getJopByIdRouter);
 app.use("/updatejop", EditJopRouter);
 app.use("/deletejop", DeletedRouter);
 app.use("/filterjop", FilterJopRouter);
-////////////
 
-app.listen(Port, async () => {
-  try {
-    connectDB();
-    console.log("Successfully connect on port", Port);
-  } catch (error) {
-    console.log("Failed Connected on port", error);
-  }
-});
+// Database Connection
+connectDB();
+
+export default app;
