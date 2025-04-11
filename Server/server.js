03e9577d6bcfc9f43138631fa56ad60644a96922
moshaosama <mohamedOSFekry@gmail.com>
2025-04-11 14:43:55 +0200

    Connect delete job route to main server file

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./ConnectDB/DB.js";
 import createJopRouter from "./Router/Jop/createJop.js";
 import { getJopByIdRouter, getJopRouter } from "./Router/Jop/getJop.js";
 import EditJopRouter from "./Router/Jop/EditJop.js";
+import DeletedRouter from "./Router/Jop/DeletedJop.js";
 const app = express();
 const Port = process.env.PORT || 3000;
 
@@ -18,6 +19,7 @@ app.use("/createjop", createJopRouter);
 app.use("/getjops", getJopRouter);
 app.use("/jop", getJopByIdRouter);
 app.use("/updatejop", EditJopRouter);
+app.use("/deletejop", DeletedRouter);
 ////////////
 
 app.listen(Port, async () => {
