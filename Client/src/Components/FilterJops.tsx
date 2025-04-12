6cf1dfb7c67276231d8979f952dee2b232ac6eb3
moshaosama <mohamedOSFekry@gmail.com>
2025-04-12 16:40:07 +0200

    Refactor and improve job filtering component UI and logic

--- Client/src/Components/FilterJops.tsx
+++ Client/src/Components/FilterJops.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import useTheme from "../Hooks/useTheme";
+import { IoMdArrowDropdown, IoMdArrowDropright } from "react-icons/io";
 
 const FilterJops = ({
   handleChange,
@@ -8,6 +10,11 @@ const FilterJops = ({
   SelectInput: string;
 }) => {
   const { theme } = useTheme();
+  const [FilterState, setFilterState] = useState(false);
+
+  const handleClick = () => {
+    setFilterState(!FilterState);
+  };
   return (
     <div
       className={`${
@@ -16,64 +23,76 @@ const FilterJops = ({
           : "bg-black text-white border-[1px]"
       } w-60 p-4 rounded-md shadow-xl`}
     >
-      <h1 className="text-xl font-bold">Filter Jops</h1>
+      <h1
+        className="text-xl font-bold flex items-center gap-3"
+        onClick={handleClick}
+      >
+        Filter Jops{" "}
+        {FilterState ? (
+          <IoMdArrowDropdown className="mt-1" />
+        ) : (
+          <IoMdArrowDropright className="mt-1" />
+        )}
+      </h1>
 
-      <div className="mt-5">
-        <h1 className="text-lg font-semibold">Status</h1>
-        <form action="" className="mt-2 flex flex-col gap-2">
-          <p className="flex gap-3">
-            <input
-              type="radio"
-              id="All"
-              name="status"
-              value="All"
-              checked={SelectInput === "All"}
-              onChange={handleChange}
-            />
-            <label htmlFor="All">All</label>
-          </p>
-          <p className="flex gap-3">
-            <input
-              type="radio"
-              id="Applied"
-              name="status"
-              value="Applied"
-              onChange={handleChange}
-            />
-            <label htmlFor="Applied">Applied</label>
-          </p>
-          <p className="flex gap-3">
-            <input
-              type="radio"
-              id="Interview"
-              name="status"
-              value="Interview"
-              onChange={handleChange}
-            />
-            <label htmlFor="Interview">Interview</label>
-          </p>
-          <p className="flex gap-3">
-            <input
-              type="radio"
-              id="Offer"
-              name="status"
-              value="Offer"
-              onChange={handleChange}
-            />
-            <label htmlFor="Offer">Offer</label>
-          </p>
-          <p className="flex gap-3">
-            <input
-              type="radio"
-              id="Rejected"
-              name="status"
-              value="Rejected"
-              onChange={handleChange}
-            />
-            <label htmlFor="Rejected">Rejected</label>
-          </p>
-        </form>
-      </div>
+      {FilterState ? (
+        <div className="mt-5">
+          <h1 className="text-lg font-semibold">Status</h1>
+          <form action="" className="mt-2 flex flex-col gap-2">
+            <p className="flex gap-3">
+              <input
+                type="radio"
+                id="All"
+                name="status"
+                value="All"
+                checked={SelectInput === "All"}
+                onChange={handleChange}
+              />
+              <label htmlFor="All">All</label>
+            </p>
+            <p className="flex gap-3">
+              <input
+                type="radio"
+                id="Applied"
+                name="status"
+                value="Applied"
+                onChange={handleChange}
+              />
+              <label htmlFor="Applied">Applied</label>
+            </p>
+            <p className="flex gap-3">
+              <input
+                type="radio"
+                id="Interview"
+                name="status"
+                value="Interview"
+                onChange={handleChange}
+              />
+              <label htmlFor="Interview">Interview</label>
+            </p>
+            <p className="flex gap-3">
+              <input
+                type="radio"
+                id="Offer"
+                name="status"
+                value="Offer"
+                onChange={handleChange}
+              />
+              <label htmlFor="Offer">Offer</label>
+            </p>
+            <p className="flex gap-3">
+              <input
+                type="radio"
+                id="Rejected"
+                name="status"
+                value="Rejected"
+                onChange={handleChange}
+              />
+              <label htmlFor="Rejected">Rejected</label>
+            </p>
+          </form>
+        </div>
+      ) : null}
     </div>
   );
 };
